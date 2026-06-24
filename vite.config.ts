21f3331463eda819cfe6b451b73b057241a5be6e
jaennil <jaennil@yandex.ru>
2026-06-24 14:05:03 +0300

    fix: allow Tuna host and add reader app icon today

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["ejm0fc-195-133-243-226.ru.tuna.am"],
+    allowedHosts: ["ejm0fc-195-133-243-226.ru.tuna.am", "br.ru.tuna.am"],
     proxy: {
       "/api": "http://127.0.0.1:8080"
     }
